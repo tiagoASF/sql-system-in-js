e0e04f7514262d36f89a4dbd7ad6f3b9ef47d355
Tiago Fialho <tiago.asf@outlook.com>
2022-07-08 07:29:46 -0300

    resolucao do exercicio2

--- sqlCommand.js
+++ sqlCommand.js
@@ -1,25 +1,64 @@
 // Objetivo
-    //Extrair partes do comando como o nome da tabela e as colunas, armazenando-as em variáveis.
+    //Com base no nome da tabela e nas colunas, monte uma estrutura de objetos para armazenar tanto a definição da tabela quanto os dados.
 
 //instruções Exercicio 1
     // Dado o comando:
         // create table author (id number, name string, age number, city string, state string, country string)
 
-        // Extraia o nome da tabela e armazene em uma variável chamada "tableName".
-        // Extraia as colunas da tabela e armazene em uma variável chamada "columns".
-        // Manipule a variável "columns", separando cada coluna com seu respectivo tipo, em uma string separada.
+        //1 - Crie um objeto chamado "database".
+        //2 - Dentro do objeto "database", crie um objeto chamado "tables".
+        //3 - Dentro do objeto "tables", crie um objeto com o nome da tabela.
+        //4 - Dentro do objeto criado com o nome da tabela, crie um objeto chamado  "columns", onde as chaves são representadas pelo nome da coluna e o valor pelo tipo.
+        //5 - Dentro do objeto criado com nome da tabela, crie um array chamado "data".
+        //6 - Exiba o conteúdo do objeto "database" utilizando JSON.stringify
 
 //Resultado esperado
-    //tableName = "author"
-    //columns = [ 'id number',' name string',' age number',' city string',' state string',' country string']
+    // {
+    //     "tables": {
+    //         "author": {
+    //             "columns": {
+    //                 "id": "number",
+    //                 "name": "string",
+    //                 "age": "number",
+    //                 "city": "string",
+    //                 "state": "string",
+    //                 "country": "string"
+    //             },
+    //             "data": []
+    //         }
+    //     }
+    // }
 
 //Dicas
-// Explore ao máximo as operações disponíveis na String API como: String.prototype.match, String.prototype.replace e String.prototype.split, juntamente com expressões regulares. A operação String.prototype.split retorna uma array, então não é necessário se preocupar em criar ou adicionar as colunas no array.
+// Percorra as colunas com for/of e utilize a notação de [] tanto para criar e acessar as propriedades nos objetos. É possível utilizar um parâmetro na operação JSON.stringify para formatar o objeto. Para isso, passe como terceiro parâmetro alguns espaços em branco ou o caracter tab.
 
 const sqlCommand = "create table author (id number, name string, age number, city string, state string, country string)";
 
-const regExp = /^create table (\w+)\s\(([\w\s,]+)\)/; 
+const regExp = /create\stable\s(\w+)\s\(([\w\s,]+)\)/;
 const result = sqlCommand.match(regExp);
 
-let tableName = result[1];
-let columns = result[2].split(", ");
+const tableName = result[1];
+let columns = result[2].split(', ');
+
+let columnsInKeyValue = [];
+for (let keyValue of columns) {
+    columnsInKeyValue.push(keyValue.split(' '));
+}
+
+let database = {
+    "tables" : {
+        [tableName] : {
+            columns : {},
+            "data" : []
+        }
+    }
+};
+
+for (let column of columnsInKeyValue) {
+    let columnName = column[0];
+    let columnValue = column[1];
+    database.tables[tableName].columns[columnName] = columnValue;
+}
+
+console.log(JSON.stringify(database, null, '\t'));
+
