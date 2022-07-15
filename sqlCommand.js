4f3751bad69eaa928d01286edbeba7b00127c61e
Tiago Fialho <tiago.asf@outlook.com>
2022-07-15 06:45:08 -0300

    enunciado

--- sqlCommand.js
+++ sqlCommand.js
@@ -1,3 +1,27 @@
+/*
+1 - Objetivo
+Lance uma exceção caso o comando não exista, interrompendo fluxo de execução
+
+2 - Instruções
+    2.1 - Crie uma função construtora chamada "DatabaseError" que recebe dois parâmetros: "statement" e "message".
+    2.2 - Dentro do método "execute", caso o comando passado por parâmetro não exista, instancie a função construtora "DatabaseError", lançando-a como um erro.
+    2.3 - Envolva a chamada para o objeto "database" em um bloco try/catch imprimindo a propriedade "message" do objeto "DatabaseError".
+
+3 - Cenário
+
+    database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+
+    database.execute("select id, name from author");
+
+4 - Resultado
+    "Syntax error: 'select id, name from author'"
+
+5 - Dicas
+Não esqueça de utilizar o operador new para instanciar a função construtora "DatabaseError" e de utilizar Template Literals para montar a mensagem de erro.
+*/
+
+
+
 let database = {
     tables : {},
     
