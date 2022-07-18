2ffdb6855c36fd13410caccdd3ef645908927220
Tiago Fialho <tiago.asf@outlook.com>
2022-07-18 07:15:34 -0300

    refatorando | excluindo linhas comentadas

--- sqlCommand.js
+++ sqlCommand.js
@@ -40,7 +40,7 @@ let database = {
     execute(query) {
         if (query.startsWith("create table")) {
             this.createTable(query);
-            console.log(JSON.stringify(database, null, "   "));
+            //console.log(JSON.stringify(database, null, "   "));
         } else if (query.startsWith("insert into")) {
             this.insert(query);
             console.log(JSON.stringify(database, null, "   "));
@@ -68,22 +68,4 @@ try {
     console.log(e.message);
 }
 
-
-
-const query = "insert into author (id, name, age) values (1, Douglas Crockford, 62)"
-
-const regExp = /^insert into\s(\w+)\s\(([\w\s,]+)\)\svalues\s\(([\w+\s,]+)\)/;
-const result = query.match(regExp);
-
-const tableName = result[1];
-const columnsNames = result[2].split(", ");
-const columnsValues = result[3].split(", ");
-const row = {};
-
-for (let i = 0; i < columnsNames.length; i++) {
-    row[columnsNames[i]] = columnsValues[i];
-    
-};
-
-database.tables[tableName].data.push(row);
 
