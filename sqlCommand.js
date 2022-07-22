4b31b61c10f6eeb67144e25d2d26e2bd3aa25e4c
Tiago Fialho <tiago.asf@outlook.com>
2022-07-22 05:42:20 -0300

    enunciado

--- sqlCommand.js
+++ sqlCommand.js
@@ -1,3 +1,40 @@
+/*
+1 - Objetivo
+    Implemente o método "delete". Para isso, é necessário extrair as informações a partir do comando e excluir os dados de acordo com a cláusula "where".
+
+2 - Instruções
+    Dada o comando:
+        delete from author where id = 2
+
+    2.1 - Crie um método chamado "delete".
+    2.2 - Na função "execute", invoque o método "delete".
+    2.3 - Extraia a cláusula where do comando.
+    2.4 - Crie as variáveis columnWhere e valueWhere.
+    2.5 - Filtre os registros conforme a cláusula where.
+    2.6 - Exclua os registros.
+
+3 - Cenário
+    database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
+    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
+    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
+    database.execute("delete from author where id = 2");
+    database.execute("select name, age from author");
+
+4 - Dicas
+    Você pode utilizar a operação Array.prototype.filter filtrar os elementos do array.
+
+5 - Resultado
+    [{
+        "name": "Douglas Crockford",
+        "age": "62"
+    }, {
+        "name": "Martin Fowler",
+        "age": "54"
+    }]
+*/
+
+
 let database = {
     tables : {},
     
